6fc92254e7b60448aa355a11b66bce455175fdd4
yuhsuan0719 <12750272@me.mcu.edu.tw>
2024-01-12 11:35:59 +0800

    week18

---
+++ week18/week18-3.py
@@ -0,0 +1,8 @@
+a = 10
+b = True
+c = 'a'
+d = 3.14
+print(a, b, c, d)
+print(a, b, c, d, sep=' ::: ', end='')
+e = input('請輸入1個字串')
+print(e)
---
+++ week18/week18-4.py
@@ -0,0 +1,13 @@
+a = 10
+print ( type(a) , a)
+a = '123'
+print( type(a) , a)
+a = 3.14
+print( type(a) ,a)
+a = 1 + 2
+print(a)
+a = 'Hello'
+b = 'World'
+print( a+b )
+print( a*5 )
+print( a[0], a[1], a[-1])
